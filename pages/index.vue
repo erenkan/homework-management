ea8c3fc0758352c94c561c348123242e067e4914
Eren Kan <erenkn@gmail.com>
2021-11-13 21:21:07 +0300

    login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,84 @@
 <template>
   <!-- <Tutorial /> -->
-  <b-container>
-    <b-button variant="success" @click="increment">Increment</b-button>
-    <p>Counter : {{ counter }}</p>
-    <b-button variant="danger" @click="decrement">Increment</b-button>
+ <b-container>
+    <div class="w-full max-w-xs">
+      <link
+        href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+        rel="stylesheet"
+      />
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="username"
+          >
+            Username
+          </label>
+          <input
+            id="username"
+            class="
+              shadow
+              appearance-none
+              border
+              rounded
+              w-full
+              py-2
+              px-3
+              text-gray-700
+              leading-tight
+              focus:outline-none focus:shadow-outline
+            "
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            class="
+              shadow
+              appearance-none
+              border border-red-500
+              rounded
+              w-full
+              py-2
+              px-3
+              text-gray-700
+              mb-3
+              leading-tight
+              focus:outline-none focus:shadow-outline
+            "
+            type="password"
+            placeholder="******************"
+          />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="
+              bg-blue-500
+              hover:bg-blue-700
+              text-white
+              font-bold
+              py-2
+              px-4
+              rounded
+              focus:outline-none focus:shadow-outline
+            "
+            type="button"
+            @click="increment"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+      <p class="text-center text-gray-500 text-xs">&copy;Smartface Inc</p>
+    </div>
   </b-container>
 </template>
 
